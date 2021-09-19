05192c1ed71603713302361e9b85dd8339de4427
Daniel Strano <stranoj@gmail.com>
2021-09-19 09:20:51 -0400

    Fix Linux platform message, and bump version

--- pyqrack/qrack_system/qrack_system.py
+++ pyqrack/qrack_system/qrack_system.py
@@ -30,7 +30,7 @@ class QrackSystem:
             shared_lib_path = "/usr/local/lib/libqrack_pinvoke.dylib"
         elif _platform == "win32":
             shared_lib_path = "C:\\Program Files\\Qrack\\bin\\qrack_pinvoke.dll"
-        else:
+        elif _platform != "linux" and _platform != "linux2":
             print("No Qrack binary for your platform, attempting to use /usr/local/lib/libqrack_pinvoke.so")
             print("You can choose the binary file to load with the environment variable: PYQRACK_SHARED_LIB_PATH")
 
