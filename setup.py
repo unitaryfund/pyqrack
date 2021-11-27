5b7feeb90c7764e93cbde39e5f647cf41d4b1a0f
Daniel Strano <stranoj@gmail.com>
2021-11-27 14:39:38 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 requirements = []
 
-VERSION = "0.7.10"
+VERSION = "0.7.11"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
