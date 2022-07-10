1d76b201012f997252a771d56feff3c81230468b
Daniel Strano <stranoj@gmail.com>
2022-07-09 22:51:50 -0400

    Revert "Bump version (v0.18.8)"
    This reverts commit 7535b652f684578ea7dcb825c44387a9d6b68028.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 requirements = []
 
-VERSION = "0.18.8"
+VERSION = "0.18.7"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
