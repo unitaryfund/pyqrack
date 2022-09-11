eb95e96e005a352dfb37386d682f5f63bac14f90
Daniel Strano <stranoj@gmail.com>
2022-09-11 09:16:37 -0400

    Bump version (v0.21.3)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 requirements = []
 
-VERSION = "0.21.2"
+VERSION = "0.21.3"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
