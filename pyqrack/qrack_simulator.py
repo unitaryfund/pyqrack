2c5abbb8371831a69db80e716fb70fe80d6d31da
Dan Strano <stranoj@gmail.com>
2023-06-24 08:55:04 -0400

    iswap_dg

--- pyqrack/qrack_simulator.py
+++ pyqrack/qrack_simulator.py
@@ -2509,7 +2509,7 @@ class QrackSimulator:
         clifford = Clifford(tableau)
         circ = clifford.to_circuit()
 
-        basis_gates = ["h", "x", "y", "z", "sx", "sy", "s", "sdg", "cx", "cy", "cz", "swap", "iswap"]
+        basis_gates = ["h", "x", "y", "z", "sx", "sy", "s", "sdg", "cx", "cy", "cz", "swap", "iswap", "iswap_dg"]
         try:
             circ = transpile(circ, basis_gates=basis_gates, optimization_level=3)
         except:
