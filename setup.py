86550473c5a790ef9cac71a5204e0e3e62bb3c92
Daniel Strano <stranoj@gmail.com>
2024-02-20 14:42:11 -0500

    v1.26.15 (Jammy 3/3)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 requirements = []
 
-VERSION = "1.26.14"
+VERSION = "1.26.15"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
