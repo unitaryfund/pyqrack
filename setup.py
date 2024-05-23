24648a5d0d32d30b32ff53c2c9dfa12d3818afae
WrathfulSpatula <stranoj@gmail.com>
2024-05-23 17:05:24 -0400

    Fix Jammy build

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 requirements = []
 
-VERSION = "1.28.4"
+VERSION = "1.28.5"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
