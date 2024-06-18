f614bb7e52d8871aa5483ad23bf93e57f850d7ab
WrathfulSpatula <stranoj@gmail.com>
2024-06-18 16:55:52 -0400

    Fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = "1.29.0"
+VERSION = "1.29.1"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
