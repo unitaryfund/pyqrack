d95935982613186e5155ac10aa4ea4a7fc61dd30
WrathfulSpatula <stranoj@gmail.com>
2024-06-19 12:50:34 -0400

    Fix Qiskit version compatibility in QrackCircuit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = "1.29.1"
+VERSION = "1.29.2"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
