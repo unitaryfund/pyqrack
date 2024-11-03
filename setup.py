c5b6931819da96fe13697595aa72d03c496d6a1c
WrathfulSpatula <stranoj@gmail.com>
2024-11-03 08:14:53 -0500

    Bump for new Qrack

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     setup_requires=[],
     extras_require={
         "dev": [
-            "pytest>=7.3.1"
+            "pytest>=7.3.1",
         ],
     },
     include_package_data=True,
