b98d41b038b77fff7922c0b4cf1d51886a45c048
WrathfulSpatula <stranoj@gmail.com>
2024-11-12 06:40:47 -0500

    Bump version (new Qrack)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = "1.32.14"
+VERSION = "1.32.15"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
