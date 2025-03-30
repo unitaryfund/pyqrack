7030d8fc0f4c18a40d61927984080dd92a3b4e07
WrathfulSpatula <stranoj@gmail.com>
2025-03-30 14:57:42 -0400

    QrackSimulator.clone()

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 
-VERSION = "1.37.0"
+VERSION = "1.38.0"
 
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
